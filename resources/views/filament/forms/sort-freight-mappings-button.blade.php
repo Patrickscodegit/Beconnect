9a9c0e4c8890b038ac1d1db2a42e5dda41d7db6d
Development <dev@example.com>
2026-01-01 13:46:58 +0100

    Fix sort button - use $wire.call() for Livewire method call

--- resources/views/filament/forms/sort-freight-mappings-button.blade.php
+++ resources/views/filament/forms/sort-freight-mappings-button.blade.php
@@ -1,7 +1,7 @@
-<div class="flex justify-end mb-4">
+<div class="flex justify-end mb-4" x-data>
     <button
         type="button"
-        wire:click="sortArticleMappingsByPort"
+        @click="$wire.call('sortArticleMappingsByPort')"
         class="inline-flex items-center justify-center gap-x-2 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm transition duration-75 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-70 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800"
     >
         <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
